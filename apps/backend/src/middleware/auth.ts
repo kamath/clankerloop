07d6de2a6688e507d2ec2c35c71738f849fda580
Anirudh Kamath <github@kamath.io>
2025-12-03 04:41:35 -0500

    add isAdmin

--- apps/backend/src/middleware/auth.ts
+++ apps/backend/src/middleware/auth.ts
@@ -1,6 +1,7 @@
 import { createMiddleware } from "hono/factory";
 import { HTTPException } from "hono/http-exception";
 import { decryptUserId } from "@/utils/auth";
+import { WorkOS } from '@workos-inc/node';
 
 export const apiKeyAuth = createMiddleware(async (c, next) => {
   const apiKey = c.req.header("X-API-Key");
@@ -13,8 +14,18 @@ export const apiKeyAuth = createMiddleware(async (c, next) => {
 
   try {
     const decryptedUserId = await decryptUserId(apiKey, c.env);
+	const workos = new WorkOS(c.env.WORKOS_API_KEY);
+
+
+	const user = await workos.userManagement.getUser(
+		decryptedUserId,
+	);
+
+	console.log("User:", JSON.stringify(user, null, 2));
+
     console.log("Decrypted user ID:", decryptedUserId);
     c.set("userId", decryptedUserId);
+	c.set("isAdmin", user.metadata?.role === 'superduperadmin');
   } catch (error) {
     console.error("Failed to decrypt user ID:", error);
     throw new HTTPException(403, {
